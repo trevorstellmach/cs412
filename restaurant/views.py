6e79718a328e9be9a50017ae19ca64f0dea27233
trevorstellmach <you@example.com>
2026-09-17 22:34:58 -0400

    finished views, updated css

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,6 +3,7 @@
 # Description: Restaurant site views file
 
 import random
+import time
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -15,9 +16,9 @@ specials = [
 
 prices = {
     "Classic Dog": 8,
-    "Ketchup": 0.5,
-    "Mustard": 0.5,
-    "Relish": 0.5,
+    "Ketchup": 0.50,
+    "Mustard": 0.50,
+    "Relish": 0.50,
     "Chicago Dog": 9,
     "Fully Loaded Dog": 11,
     "Short Dog": 7,
@@ -49,26 +50,53 @@ def confirmation(request):
 
     print(request.POST)
 
-    cost = 0
-    food_order = []
-    toppings = []
-    instructs = ""
-    name = ""
-    number = ""
-    email = ""
+    cost = 0        # total cost
+    food_order = [] # list of ordered food items
+    toppings = []   # list of ordered toppings
 
-    # check if POST data exists
-    #if request.POST:
 
     # pull form fields into data
     for x in request.POST:
-        print(request.POST[x])
+        if x == 'csrfmiddlewaretoken':      # skip first in dictionary
+            continue
+        if x == "special_instructions":     # break at end of food items
+            break
+        if x == "Ketchup" or x == "Mustard" or x == "Relish":  # build 'toppings' list
+            toppings += [x]
+        else:
+            food_order += [x]       # build 'food_order' list
 
 
-    #context = {
+    # add cost of food items to total cost
+    for x in food_order:
+        cost += prices[x]
+
+    # add cost of toppings to total cost
+    for x in toppings:
+        cost += prices[x]
+
+    # if no toppings, provide 'no toppings' message
+    if toppings == []:
+        toppings = ["No toppings"]
+
+    # select ready time 30-60 minutes from current time
+    readytime = time.time() + random.randint(1800, 3600)
+    readytime = time.ctime(readytime)
+
+    # context variables
+    context = {
+        "instructs": request.POST["special_instructions"],
+        "name": request.POST["name"],
+        "number": request.POST["number"],
+        "email": request.POST["email"],
+        "food_order": food_order,
+        "toppings": toppings,
+        "cost": cost,
+        "readytime": readytime
+    }
 
     
-
+    # html template file
     template_name = "restaurant/confirmation.html"
 
-    return render(request, template_name)
+    return render(request, template_name, context)
